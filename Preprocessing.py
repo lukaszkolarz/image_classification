b7919a981fbf1db8fdedfc19a14223251ce6b1b8
lukaszkolarz <lukaszkolarz@gmail.com>
2020-12-21 22:57:26 +0100

    added dataset import module

---
+++ Preprocessing.py
@@ -0,0 +1,20 @@
+import Augmentor
+
+
+def prepare_data(source, output):
+    aug = Augmentor.Pipeline(source_directory=source, output_directory=output)
+    aug.flip_top_bottom(0.3)
+    aug.flip_left_right(0.25)
+    aug.zoom(probability=0.2, min_factor=1.1, max_factor=1.7)
+    aug.rotate(probability=0.6, max_left_rotation=15, max_right_rotation=15)
+    aug.sample(40000, multi_threaded=True)
+
+
+if __name__ == '__main__':
+    # prepare_data('/Users/lukaszkolarz/Desktop/AGH/praca inżynierska/dataset.nosync/train',
+    #              '/Users/lukaszkolarz/Desktop/AGH/praca inżynierska/dataset.nosync/train_augmeted')
+
+    resize = Augmentor.Pipeline('/Users/lukaszkolarz/Desktop/AGH/praca inżynierska/dataset.nosync/train_augmeted',
+                                '/Users/lukaszkolarz/Desktop/AGH/praca inżynierska/dataset.nosync/train_augmeted_resized')
+    resize.resize(probability=1.0, width=496, height=496)
+    resize.process()
--- main.py
+++ main.py
@@ -9,22 +9,38 @@ import ImageLoader
 
 batch_size = 64
 epochs = 2
-num_classes = 10
-learning_rate = 1e-5
-classes = 2
+learning_rate = 1e-3
+classes = 4
 
 #loader = ImageLoader.ImageLoader(train_path="/Users/lukaszkolarz/Downloads/OCT2017/proba/",
 #                                 size=(496, 512))
-(x_train, y_train), (x_test, y_test) = datasets.cifar10.load_data()
+#(x_train, y_train), (x_test, y_test) = datasets.cifar10.load_data()
 #(x_train, y_train), (x_test, y_test) = loader.import_images()
 
 
-x_train = x_train / 255
-x_test = x_test / 255
+#x_train = x_train / 255
+#x_test = x_test / 255
 
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
-input_shape = (x_train.shape[1], x_train.shape[2], x_train.shape[3])
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "/Users/lukaszkolarz/Desktop/AGH/praca inżynierska/dataset.nosync/train_augmeted_resized",
+    validation_split=0.1,
+    subset="training",
+    seed=123,
+    image_size=(496, 496),
+    batch_size=128)
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+      "/Users/lukaszkolarz/Desktop/AGH/praca inżynierska/dataset.nosync/validation",
+      validation_split=0.1,
+      subset="validation",
+      seed=123,
+      image_size=(496, 496),
+      batch_size=128)
+
+print(train_ds, 'train samples')
+#print(x_test.shape[0], 'test samples')
+input_shape = (496, 496, 3)
 
 #model = ResNet34.ResNet34(input_shape, classes)
 #model = ResNet50.ResNet50(input_shape, classes)
@@ -33,11 +49,11 @@ model = SEResNet50.SEResNet50(input_shape, classes)
 model.summary()
 
 model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate),
-              loss='binary_crossentopy',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['acc'])
 
-history = model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(x_test, y_test))
-#print(history.history)
+history = model.fit(train_ds, epochs=epochs, batch_size=batch_size, validation_data=val_ds)
+print(history.history)
 
 #test_loss, test_acc = model.evaluate(x_test, y_test)
 #print(test_acc)
