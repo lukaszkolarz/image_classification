c492418dcb00746b9bcf5fed88d45f52073ff665
lukaszkolarz <lukaszkolarz@gmail.com>
2020-12-22 22:56:46 +0100

    some improvements

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 import plot_result as plot
 #import ImageLoader
 
-batch_size = 16
+batch_size = 64
 epochs = 2
 learning_rate = 1e-3
 classes = 4
@@ -31,15 +31,15 @@ classes = 4
 #       image_size=(496, 496),
 #       batch_size=128)
 
-data_generator = tf.keras.preprocessing.image.ImageDataGenerator()
+data_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
 train_ds = data_generator.flow_from_directory('/Users/lukaszkolarz/Desktop/AGH/praca inżynierska/dataset.nosync/train_augmeted_resized',
-                                              class_mode='categorical', batch_size=batch_size)
+                                              class_mode='categorical', batch_size=batch_size, target_size=(496, 496))
 val_ds = data_generator.flow_from_directory('/Users/lukaszkolarz/Desktop/AGH/praca inżynierska/dataset.nosync/validation',
-                                            class_mode='categorical', batch_size=batch_size)
+                                            class_mode='categorical', batch_size=batch_size, target_size=(496, 496))
 
-print(train_ds, 'train samples')
+#print(train_ds, 'train samples')
 #print(x_test.shape[0], 'test samples')
-input_shape = (496, 496, 3)
+input_shape = (496, 496, 1)
 
 #model = ResNet34.ResNet34(input_shape, classes)
 #model = ResNet50.ResNet50(input_shape, classes)
@@ -48,11 +48,11 @@ model = SEResNet50.SEResNet50(input_shape, classes)
 model.summary()
 
 model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate),
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              loss='categorical_crossentropy',
               metrics=['acc'])
 
 #history = model.fit(train_ds, epochs=epochs, batch_size=batch_size, validation_data=val_ds)
-history = model.fit(train_ds, steps_per_epoch=10, validation_steps=2, epochs=epochs)
+history = model.fit(train_ds, steps_per_epoch=10, validation_steps=1, epochs=epochs, validation_data=val_ds)
 print(history.history)
 
 #test_loss, test_acc = model.evaluate(x_test, y_test)
