29432a86f1a27f0a5e4f3c7140052cd24eee4abf
lukaszkolarz <lukaszkolarz@gmail.com>
2020-12-22 20:45:32 +0100

    image loader added

--- main.py
+++ main.py
@@ -1,42 +1,41 @@
-from tensorflow.keras import datasets
 import ResNet34
 import ResNet50
 import SEResNet50
 import numpy as np
 import tensorflow as tf
 import plot_result as plot
-import ImageLoader
+#import ImageLoader
 
-batch_size = 64
+batch_size = 16
 epochs = 2
 learning_rate = 1e-3
 classes = 4
 
 #loader = ImageLoader.ImageLoader(train_path="/Users/lukaszkolarz/Downloads/OCT2017/proba/",
 #                                 size=(496, 512))
-#(x_train, y_train), (x_test, y_test) = datasets.cifar10.load_data()
-#(x_train, y_train), (x_test, y_test) = loader.import_images()
 
 
-#x_train = x_train / 255
-#x_test = x_test / 255
+# train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+#     "/Users/lukaszkolarz/Desktop/AGH/praca inżynierska/dataset.nosync/train_augmeted_resized",
+#     validation_split=0.1,
+#     subset="training",
+#     seed=123,
+#     image_size=(496, 496),
+#     batch_size=128)
+#
+# val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+#       "/Users/lukaszkolarz/Desktop/AGH/praca inżynierska/dataset.nosync/validation",
+#       validation_split=0.1,
+#       subset="validation",
+#       seed=123,
+#       image_size=(496, 496),
+#       batch_size=128)
 
-
-train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    "/Users/lukaszkolarz/Desktop/AGH/praca inżynierska/dataset.nosync/train_augmeted_resized",
-    validation_split=0.1,
-    subset="training",
-    seed=123,
-    image_size=(496, 496),
-    batch_size=128)
-
-val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-      "/Users/lukaszkolarz/Desktop/AGH/praca inżynierska/dataset.nosync/validation",
-      validation_split=0.1,
-      subset="validation",
-      seed=123,
-      image_size=(496, 496),
-      batch_size=128)
+data_generator = tf.keras.preprocessing.image.ImageDataGenerator()
+train_ds = data_generator.flow_from_directory('/Users/lukaszkolarz/Desktop/AGH/praca inżynierska/dataset.nosync/train_augmeted_resized',
+                                              class_mode='categorical', batch_size=batch_size)
+val_ds = data_generator.flow_from_directory('/Users/lukaszkolarz/Desktop/AGH/praca inżynierska/dataset.nosync/validation',
+                                            class_mode='categorical', batch_size=batch_size)
 
 print(train_ds, 'train samples')
 #print(x_test.shape[0], 'test samples')
@@ -52,7 +51,8 @@ model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate),
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['acc'])
 
-history = model.fit(train_ds, epochs=epochs, batch_size=batch_size, validation_data=val_ds)
+#history = model.fit(train_ds, epochs=epochs, batch_size=batch_size, validation_data=val_ds)
+history = model.fit(train_ds, steps_per_epoch=10, validation_steps=2, epochs=epochs)
 print(history.history)
 
 #test_loss, test_acc = model.evaluate(x_test, y_test)
