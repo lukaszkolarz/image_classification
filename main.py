f6b576ee3c2c7432d9635fd0f08ddacfda73fcb5
lukaszkolarz <lukaszkolarz@gmail.com>
2020-12-27 20:50:57 +0100

    some improvements

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from DataLoader import DataGenerator
 
 batch_size = 32
 epochs = 2
-learning_rate = 1e-4
+learning_rate = 1e-3
 classes = 4
-target_size = 700
+target_size = 500
 
 train_ds, val_ds, input_shape = DataGenerator.import_greyscale(target_size=target_size,
                                                                batch_size=batch_size,
@@ -24,12 +24,12 @@ lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_ra
                                                              staircase=True)
 
 with strategy.scope():
-    model = ResNet34.ResNet34(input_shape, classes)
-    #model = ResNet50.ResNet50(input_shape, classes)
+    #model = ResNet34.ResNet34(input_shape, classes)
+    model = ResNet50.ResNet50(input_shape, classes)
     #model = SEResNet50.SEResNet50(input_shape, classes)
     #model = TestNet.testNet(input_shape, classes)
 
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate),
+    model.compile(optimizer=tf.keras.optimizers.Adam(lr_schedule),
                   loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
                   metrics=['acc'])
 
