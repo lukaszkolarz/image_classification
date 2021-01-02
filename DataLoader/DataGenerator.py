20e0b35c30ef8ff2d5b5eafef4f2d642247308b4
lukaszkolarz <lukaszkolarz@gmail.com>
2021-01-02 12:37:42 +0100

    some improvements added

--- DataLoader/DataGenerator.py
+++ DataLoader/DataGenerator.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 
-def import_greyscale(target_size, batch_size, source_train, source_val):
+def import_greyscale(target_size, batch_size, source_train, source_val, source_test):
     train_data_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255,
                                                                            zoom_range=0.0,
                                                                            rotation_range=0,
@@ -10,15 +10,23 @@ def import_greyscale(target_size, batch_size, source_train, source_val):
                                                         class_mode='categorical',
                                                         batch_size=batch_size,
                                                         color_mode='grayscale',
-                                                        target_size=(target_size, target_size))
+                                                        target_size=(target_size, target_size),
+                                                        shuffle=True)
 
     test_data_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
     val_ds = test_data_generator.flow_from_directory(source_val,
                                                      class_mode='categorical',
                                                      batch_size=batch_size,
                                                      color_mode='grayscale',
-                                                     target_size=(target_size, target_size))
+                                                     target_size=(target_size, target_size),
+                                                     shuffle=True)
+    test_ds = test_data_generator.flow_from_directory(source_test,
+                                                      class_mode='categorical',
+                                                      batch_size=batch_size,
+                                                      color_mode='grayscale',
+                                                      target_size=(target_size, target_size),
+                                                      shuffle=True)
 
     input_shape = (target_size, target_size, 1)
 
-    return train_ds, val_ds, input_shape
+    return train_ds, val_ds, test_ds, input_shape
--- main.py
+++ main.py
@@ -3,17 +3,17 @@ import tensorflow as tf
 import plot_result as plot
 from DataLoader import DataGenerator
 
-
 batch_size = 8
-epochs = 35
+epochs = 32
 learning_rate = 1e-3
 classes = 4
 target_size = 800
 
-train_ds, val_ds, input_shape = DataGenerator.import_greyscale(target_size=target_size,
-                                                               batch_size=batch_size,
-                                                               source_train='/net/people/plgkolarzl/dataset',
-                                                               source_val='/net/people/plgkolarzl/test')
+train_ds, val_ds, test_ds, input_shape = DataGenerator.import_greyscale(target_size=target_size,
+                                                                        batch_size=batch_size,
+                                                                        source_train='/net/archive/groups/plggmlkt/dataset/train',
+                                                                        source_val='/net/archive/groups/plggmlkt/dataset/validation',
+                                                                        source_test='/net/archive/groups/plggmlkt/dataset/test')
 
 strategy = tf.distribute.MirroredStrategy()
 print('Number of GPUs: {}'.format(strategy.num_replicas_in_sync))
@@ -24,11 +24,11 @@ lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_ra
                                                              staircase=True)
 
 with strategy.scope():
-    #model = ResNet34.ResNet34(input_shape, classes)
-    #model = ResNet50.ResNet50(input_shape, classes)
-    #model = SEResNet50.SEResNet50(input_shape, classes)
-    #model = TestNet.testNet(input_shape, classes)
-    #model = tf.keras.applications.DenseNet121(classes=4, weights=None)
+    # model = ResNet34.ResNet34(input_shape, classes)
+    # model = ResNet50.ResNet50(input_shape, classes)
+    # model = SEResNet50.SEResNet50(input_shape, classes)
+    # model = TestNet.testNet(input_shape, classes)
+    # model = tf.keras.applications.DenseNet121(classes=4, weights=None)
     model = DenseNet121.DenseNet121(input_shape, classes)
 
     model.compile(optimizer=tf.keras.optimizers.Adam(lr_schedule),
@@ -36,13 +36,13 @@ with strategy.scope():
                   metrics=['acc'])
 
 model.summary()
-#history = model.fit(train_ds, epochs=epochs, batch_size=batch_size, validation_data=val_ds)
+# history = model.fit(train_ds, epochs=epochs, batch_size=batch_size, validation_data=val_ds)
 history = model.fit(train_ds, steps_per_epoch=3500, epochs=epochs, validation_data=val_ds, validation_steps=100)
-#print(history.history)
+# print(history.history)
 
-#test_loss, test_acc = model.evaluate(x_test, y_test)
-#print(test_acc)
+test_loss, test_acc = model.evaluate(test_ds, steps=14)
+print('Test accuracy: ' + str(test_acc))
+print('Test loss: ' + str(test_loss))
 
-#tf.keras.utils.plot_model(model, to_file='SEResNet50_architecture.png', show_shapes=False, show_layer_names=False)
+# tf.keras.utils.plot_model(model, to_file='SEResNet50_architecture.png', show_shapes=False, show_layer_names=False)
 plot.plot_history(history, 'results.png')
-
